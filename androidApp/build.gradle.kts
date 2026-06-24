c09087256ac0447c2f747c8537d3cfb75084d28e
トカルスキーP <katachionsha@outlook.jp>
2026-06-24 16:35:49 -0300

    Create build.gradle.kts

---
+++ androidApp/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.sdercolin.vlabeler.android"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.sdercolin.vlabeler.android"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = project.version.toString()
+    }
+
+    buildFeatures { compose = true }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.13" }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions { jvmTarget = "11" }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.documentfile:documentfile:1.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+}
